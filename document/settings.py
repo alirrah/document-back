5ab15ab09732c47477fbfebf0c6a36a4eceeac38
alirrah <alir.rah81@gmail.com>
2024-09-25 15:15:41 +0330

    [ADD] url for static files

--- document/settings.py
+++ document/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,6 +85,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
+]
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # برای استفاده در تولید
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # مسیر به پوشه استاتیک
 ]
 
 LANGUAGE_CODE = "en-us"
--- document/urls.py
+++ document/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+import os
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("jwt/", include("authentication.urls")),
     path("document/", include("hub.urls")),
 ]
+
+urlpatterns += static(
+    settings.STATIC_URL, document_root=os.path.join(settings.BASE_DIR, "static")
+)
